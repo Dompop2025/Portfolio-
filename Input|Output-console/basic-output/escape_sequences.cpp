207e8d41d00c727c29f1c47181cdb4546b4155a0
Bobrov <Danilbbs21@yandex.ru>
2025-12-03 00:08:08 +0700

    Add files via upload

---
+++ Input|Output-console/basic-output/escape_sequences.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Column1\tColumn2\tColumn3\n";
+    std::cout << "Data1\tData2\tData3\n";
+    std::cout << "He said: \"Hello!\"\n";
+    std::cout << "Path: C:\\Users\\Bobrov\n";
+    return 0;
+}
